a246e4e54159d75a8beb1a76241d2fc6353dfe8b
matthewbell <matthew.bell@mymail.champlain.edu>
2012-02-01 14:55:59 -0500

    second commit

---
+++ gitTest1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gitTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a test project for gitHub.com
+        }
+    }
+}
